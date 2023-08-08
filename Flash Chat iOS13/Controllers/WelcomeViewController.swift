052b3d7b58ff7c6f8a214ff9ad1dcc628c7768b6
chi chan <chitsuenchan@gmail.com>
2023-08-08 15:56:05 +0800

    removed CLTyping pod but causes an error

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
-import CLTypingLabel
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController: UILabel {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = "⚡️FlashChat"
+        
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        var charIndex = 0.0
+        
+        for letter in titleText {
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            
+            charIndex += 1
+        }
+        
+        
         
        
     }
