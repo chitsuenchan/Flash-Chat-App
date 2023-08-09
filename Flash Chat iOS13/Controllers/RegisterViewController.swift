01d031357a22e30d50c9a7f3985f702d0b2233ca
chi chan <chitsuenchan@gmail.com>
2023-08-09 14:45:49 +0800

    added firebase pod to project, added register capability, added login capability

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import FirebaseAuth
+ 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        print("hello")
+       
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription) // This displays the error message. localizedDescription is the just the error string in the device language.
+                } else {
+                    //Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
